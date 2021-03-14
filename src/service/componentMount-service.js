6906b7f81de0d0f4b30f6e9d6a523253d2fb46e3
topher-dunlap <thelonioustopher@yahoo.com>
2021-03-14 17:26:13 -0500

    removing https

--- src/service/componentMount-service.js
+++ src/service/componentMount-service.js
@@ -14,7 +14,7 @@ const ComponentMountService = {
     getUserLocation() {
         const config = {
             method: 'get',
-            url: `https://ip-api.com/json/`,
+            url: `http://ip-api.com/json/`,
         };
         return axios(config)
     },
